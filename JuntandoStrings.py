fb246741fd7739cbbe710cf0d8c184eb141b4e61
JMCalizario <35472547+JMCalizario@users.noreply.github.com>
2021-06-08 10:39:35 -0300

    Add files via upload

---
+++ JuntandoStrings.py
@@ -0,0 +1,13 @@
+# Capitulo 1
+# chat bot em ingles
+
+print('Learning Inglish Names ')
+print('-' * 40)
+print('hi  !!! i am a roobot, nice to meet you :) ')
+char1 = (input('what is your first name ?'))
+char2 = (input('ok, so  what is your middle name ?'))
+print('Nice :) !!!')
+char3 = input('what is your  last name ?')
+print('Thanks !!! ')
+full_name = char1 + char2 + char3
+print('Your full name is : ', full_name.rstrip())
